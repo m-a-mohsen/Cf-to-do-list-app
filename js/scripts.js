0e868ffc854fbdf6246c671e848240cca0e9b8aa
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2022-10-06 23:53:50 +0200

    Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,92 +1,43 @@
-
-
-function newItem(){
-
-    //javascript
+function newItem() {
+    ////////////////////////////////////////////////
+    // JQ 1.0
     //1. Adding a new item to the list of items: 
-       let li = document.createElement("li");
-       let inputValue = document.getElementById("input").value;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
-    
-       if (inputValue === '') {
-         alert("You must write something!");
-       } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
-       }
-    // 
-     //2. Crossing out an item from the list of items:
-       function crossOut() {
-             li.classList.toggle("strike");
-         }
-    
-         li.addEventListener("dblclick",crossOut);
-    
-     //3(i). Adding the delete button "X": 
-       let crossOutButton = document.createElement("crossOutButton");
-         crossOutButton.appendChild(document.createTextNode("X"));
-         li.appendChild(crossOutButton);
-    
-         crossOutButton.addEventListener("click", deleteListItem);
-     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-       function deleteListItem(){
-             li.classList.add("delete")
-         }
-     // 4. Reordering the items: 
-       $('#list').sortable();
-    
-    }
-    
-     
-    
-    
-    
-    
-    
-    
-    
-    // If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
-    
-    
-    
-    
-    
-    
-    
-    
-      /*
-    // jQuery Code
-    //1. Adding a new item to the list:
-    
-      let li = $('<li></li>');
-      let inputValue = $('#input').val();
-      li.append(inputValue);
-    
-      if (inputValue === '') {
+    let jqinputValue = $('#input').val();
+    console.log(jqinputValue);
+    let jqli = $('<li></li>');
+    jqli.append(jqinputValue);
+
+    if (jqinputValue === '') {
         alert("You must write something!");
-      } else {
-        $('#list').append(li);
-      }
-    //2. Crossing an item out:
-      function crossOut() {
-            li.toggleClass("strike");
-        }
-    
-        li.on("dblclick", function crossOut() {
-            li.toggleClass("strike");
-        });
-    //3. Adding a delete button
-      let crossOutButton = $('<crossOutButton></crossOutButton>');
-      crossOutButton.append(document.createTextNode('X'));
-      li.append(crossOutButton);
-    
-    //   crossOutButton.on("click", deleteListItem);
-    //   function deleteListItem(){
-    // 		li.addClass("delete")
-    // 	}
-       $('#list').sortable();
-    */
-    
-    
-    
+    } else {
+        $('#list').append(jqli);
+    }
+    ///////////////////////////////////////////////////////////////
+    // JQ 2.0
+    //2. Crossing out an item from the list of items:
+
+    jqli.on("dblclick", function crossOut() {
+        jqli.toggleClass("strike");
+    });
+
+    ////////////////////////////////////////////////////////////////
+    // JQ 3.0
+    //3(i). Adding the delete button "X": 
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    jqli.append(crossOutButton);
+
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+
+    function deleteListItem() {
+        jqli.addClass("delete")
+    }
+    crossOutButton.on("click", deleteListItem);
+
+    /////////////////////////////////////////////////////////////////
+    // JQ 4.0
+    // 4. Reordering the items: 
+    $('#list').sortable();
+
+}
+
